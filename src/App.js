629cbc5a46c5456dea855fa2a59cf2a39570c8be
Carlo <thecarloferrer@gmail.com>
2019-04-07 16:12:59 -0400

    Correct errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Summary from './sections/summary';
+import Summary from './summary/summary';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
