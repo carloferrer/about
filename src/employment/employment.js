8da7c14ee31d18b50a1ac27783ebeed6e3d3bd3d
Carlo <thecarloferrer@gmail.com>
2019-04-06 23:04:59 -0400

    Issue #1: Create basic components
    - Create basic contact, education, employment, projects, skills, and summmary components
    - Bring components into a single resume component
    - Add SASS as a dependency

---
+++ src/employment/employment.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './employment.scss';
+
+class Emplyoment extends Component {
+  render() {
+    return <div>Emplyoment yo.</div>;
+  }
+}
+
+export default Emplyoment;
---
+++ src/summary/summary.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './summary.scss';
+
+class Summary extends Component {
+  render() {
+    return <div>Summary yo.</div>;
+  }
+}
+
+export default Summary;
