b653a36c267f2a5a6f89cb18730fa652d5034446
Carlo <thecarloferrer@gmail.com>
2019-04-08 16:50:24 -0400

    Add most of education
    Consider adding an additional section detailing sections further.

--- src/education/education.js
+++ src/education/education.js
@@ -3,7 +3,34 @@ import './education.scss';
 
 class Education extends Component {
   render() {
-    return <div className="Education">Education yo.</div>;
+    const educationList = [
+      {
+        institution: 'Lighthouse Labs',
+        discipline: 'Web Development Bootcamp',
+        graduationYear: '2017',
+      },
+      {
+        institution: "Queen's University",
+        discipline: 'Mechanical Engineering',
+        graduationYear: '2015',
+      },
+    ];
+
+    const educationNode = educationList.map((education) => <li>
+      <hgroup>
+        <h3>{education.institution}</h3>
+        <h4>{education.discipline}</h4>
+        <h5>{education.graduationYear}</h5>
+      </hgroup>
+    </li>);
+
+    return (
+      <section className="Education">
+        <ol>
+          {educationNode}
+        </ol>
+      </section>
+    );
   }
 }
 
