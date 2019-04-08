c01fd7fb0e0d6e3f54532ac7b6e440980ea69a12
Carlo <thecarloferrer@gmail.com>
2019-04-08 09:52:35 -0400

    Issue #3: Add content
    - Add rough content for all sections
    - Add a splash of colour

--- src/education/education.js
+++ src/education/education.js
@@ -3,7 +3,35 @@ import './education.scss';
 
 class Education extends Component {
   render() {
-    return <div>Education yo.</div>;
+    const educationList = [
+      {
+        institution: 'Lighthouse Labs',
+        discipline: 'Web Development Bootcamp',
+        graduationYear: '2017',
+      },
+      {
+        institution: "Queen's University",
+        discipline: 'Mechanical Engineering',
+        graduationYear: '2015',
+      },
+    ];
+
+    const educationNode = educationList.map((education) => <li>
+      <hgroup>
+        <h3>{education.institution}</h3>
+        <h4>{education.discipline}</h4>
+        <h5>{education.graduationYear}</h5>
+      </hgroup>
+    </li>);
+
+    return (
+      <section className="Education">
+        <h2>Education</h2>
+        <ol>
+          {educationNode}
+        </ol>
+      </section>
+    );
   }
 }
 
--- src/employment/employment.js
+++ src/employment/employment.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
 import './employment.scss';
 
-class Emplyoment extends Component {
+class Employment extends Component {
   render() {
-    return <div>Emplyoment yo.</div>;
+    const employmentList = [
+      {
+        employer: 'GroupBy Inc.',
+        position: 'StoreFront Developer',
+        location: 'Toronto, ON',
+        dateStart: 'Feb 2019',
+        dateEnd: 'Apr 2019',
+        duties: 'StoreFront development and stuff I guess.',
+      },
+      {
+        employer: 'GroupBy Inc.',
+        position: 'Front End Consultant',
+        location: 'Toronto, ON',
+        dateStart: 'Nov 2017',
+        dateEnd: 'Feb 2019',
+        duties: 'Front end stuff.',
+      },
+      {
+        employer: 'Consolidated Energy Solutions Inc.',
+        position: 'Design Engineer (EIT)',
+        location: 'Toronto, ON',
+        dateStart: 'Jul 2017',
+        dateEnd: 'Sep 2015',
+        duties: 'HVAC systems design and whatnot.',
+      },
+      {
+        employer: 'GE-Hitachi Nuclear Energy Canada Inc.',
+        position: 'Mechanical Engineering Intern',
+        location: 'Peterborough, ON',
+        dateStart: 'May 2013',
+        dateEnd: 'Aug 2014',
+        duties: 'Mechanical design and analysis for nuclear stuff.',
+      }
+    ];
+
+    const employmentNode = employmentList.map((employment) => <li>
+      <hgroup>
+        <h3>{employment.employer}</h3>
+        <h5>{employment.location}</h5>
+        <h5>{employment.dateStart} to {employment.dateEnd}</h5>
+        <h4>{employment.position}</h4>
+      </hgroup>
+      <p>{employment.duties}</p>
+    </li>);
+
+    return (
+      <section className="Employment">
+        <h2>Employment</h2>
+        <ol>
+          {employmentNode}
+        </ol>
+      </section>
+    );
   }
 }
 
-export default Emplyoment;
+export default Employment;
