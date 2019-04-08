a7ae5352e2410d980184a903360bd30618d00381
Carlo <thecarloferrer@gmail.com>
2019-04-08 12:00:12 -0400

    Add skills

--- src/skills/skills.js
+++ src/skills/skills.js
@@ -3,7 +3,32 @@ import './skills.scss';
 
 class Skills extends Component {
   render() {
-    return <div className='Skills'>Skills yo.</div>;
+    const skillsLists = {
+      Languages: ['JavaScript', 'HTML', 'CSS/SCSS/SASS', 'Bash', 'MATLAB', 'C', 'VBA'],
+      'Frameworks & Libraries': ['Riot.js', 'React', 'Angular', 'Express.js', 'jQuery'],
+      Testing: ['Mocha', 'Chai', 'Sinon'],
+      Databases: ['MongoDB', 'PostgreSQL'],
+      Workflow: ['JIRA', 'Node.js', 'MS Word', 'MS Excel', 'MS PowerPoint'],
+      'CAD Modelling': ['AutoCAD', 'Inventor', 'SOLIDWORKS', 'Solid Edge']
+    };
+
+    const skillsNode = Object.keys(skillsLists).map((listTitle) => {
+      const list = skillsLists[listTitle].map((item) => <li>{item}</li>);
+
+      return <li>
+        <h3>{listTitle}</h3>
+        <ul>{list}</ul>
+      </li>
+    });
+
+    return (
+      <section className='Skills'>
+        <h2>Skills</h2>
+        <ul>
+          {skillsNode}
+        </ul>
+      </section>
+    );
   }
 }
 
