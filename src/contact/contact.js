d179913b1fb77f164441e2a72216e241f5cba8fa
Carlo <thecarloferrer@gmail.com>
2019-04-08 10:01:14 -0400

    Add contact info

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -3,7 +3,18 @@ import './contact.scss';
 
 class Contact extends Component {
   render() {
-    return <div className="Contact">Contact yo.</div>;
+    return (
+      <address className="Contact">
+      <h2>Contact Information</h2>
+        <ul>
+          <li>thecarloferrer@gmail.com</li>
+          <li>647 331 6441</li>
+          <li>ca.linkedin.com/in/carloferrer</li>
+          <li>github.com/carloferrer</li>
+          <li>Toronto, ON (Downtown)</li>
+        </ul>
+      </address>
+    );
   }
 }
 
