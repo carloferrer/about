4f77ee0956a969498553eb0bb1d3ce9161b4a99b
Carlo <thecarloferrer@gmail.com>
2019-04-08 21:43:20 -0400

    Remove break tag

--- src/summary/summary.js
+++ src/summary/summary.js
@@ -9,7 +9,6 @@ class Summary extends Component {
         <p>
           Mechanical engineer turned web developer with: 1.5 years in e-commerce as a front-end developer; 2 years in HVAC and 1.5 years in nuclear power generation as an engineer.
         </p>
-        <br/>
         <p>
           I had a desire for generating creative solutions that exceeded the pace of the mechanical engineering industry, so the transition into the tech industry was only natural.  I'm a proven quick learner, natural problem solver, and obsessed with optimization (e.g., DRY code).
         </p>
