47e18e340c9cb6692af90bd34f6cad096b45e9e4
Carlo <thecarloferrer@gmail.com>
2019-04-08 21:41:04 -0400

    Fix HTML semantics in summary

--- src/summary/summary.js
+++ src/summary/summary.js
@@ -6,13 +6,13 @@ class Summary extends Component {
     return (
       <section className="Summary">
         <h2>Summary</h2>
-        <section>
+        <p>
           Mechanical engineer turned web developer with: 1.5 years in e-commerce as a front-end developer; 2 years in HVAC and 1.5 years in nuclear power generation as an engineer.
-        </section>
+        </p>
         <br/>
-        <section>
+        <p>
           I had a desire for generating creative solutions that exceeded the pace of the mechanical engineering industry, so the transition into the tech industry was only natural.  I'm a proven quick learner, natural problem solver, and obsessed with optimization (e.g., DRY code).
-        </section>
+        </p>
       </section>
     );
   }
