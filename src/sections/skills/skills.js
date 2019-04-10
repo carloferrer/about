63b16b7307f5b94da2061f3d8b1d5d560d40af06
Carlo <thecarloferrer@gmail.com>
2019-04-10 18:43:43 -0400

    Apply state to skills component

--- src/sections/skills/skills.js
+++ src/sections/skills/skills.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
+import content from '../../content';
 import './skills.scss';
 
 class Skills extends Component {
-  render() {
-    const skillsLists = {
-      Languages: ['JavaScript', 'HTML', 'CSS/SCSS/SASS', 'Bash', 'MATLAB', 'C', 'VBA'],
-      'Frameworks & Libraries': ['Riot.js', 'React', 'Angular', 'Express.js', 'jQuery'],
-      Testing: ['Mocha', 'Chai', 'Sinon'],
-      Databases: ['MongoDB', 'PostgreSQL'],
-      Workflow: ['JIRA', 'Node.js', 'MS Word', 'MS Excel', 'MS PowerPoint'],
-      'CAD Modelling': ['AutoCAD', 'Inventor', 'SOLIDWORKS', 'Solid Edge']
-    };
+  constructor() {
+    super();
 
-    const skillsNode = Object.keys(skillsLists).map((listTitle) => {
-      const list = skillsLists[listTitle].map((item) => <li>{item}</li>);
+    this.state = {
+      skills: content.skills,
+    };
+  }
+
+  render() {
+    const skillsItems = Object.keys(this.state.skills).map((listTitle) => {
+      const list = this.state.skills[listTitle].map((item) => <li>{item}</li>);
 
       return <li>
         <h3>{listTitle}</h3>
@@ -24,9 +24,7 @@ class Skills extends Component {
     return (
       <section className='Skills'>
         <h2>Skills</h2>
-        <ul>
-          {skillsNode}
-        </ul>
+        <ul>{skillsItems}</ul>
       </section>
     );
   }
