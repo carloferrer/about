f3995f90176266c461e61042c53db0881b5de79d
Carlo <thecarloferrer@gmail.com>
2019-04-11 12:14:52 -0400

    Thursday | 2019-04-11 | 12:14:44

---
+++ precache-manifest.acbee44dcdb6d2d85cf7537cc16e8e5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9d14ce69e14ac8f8bff",
+    "url": "/about/static/js/runtime~main.b6ae1bd1.js"
+  },
+  {
+    "revision": "1165bc0a4a000bb86200",
+    "url": "/about/static/js/main.0211a1f7.chunk.js"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/about/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "1165bc0a4a000bb86200",
+    "url": "/about/static/css/main.e97fe048.chunk.css"
+  },
+  {
+    "revision": "d6f01f6ea5ea57801e8bf99abd30319f",
+    "url": "/about/index.html"
+  }
+];
