0e7dc837717b02eaf64f27f88969548416a46ab2
Carlo <thecarloferrer@gmail.com>
2019-04-11 18:06:20 -0400

    Apply desktop-only style

--- src/resume.js
+++ src/resume.js
@@ -7,16 +7,18 @@ class Resume extends Component {
   render() {
     return (
       <main className="Resume">
-        <div className="desktop-left">
-          <Section.Header content={ content }/>
-          <Section.Employment content={ content }/>
-          <Section.Projects content={ content }/>
-        </div>
-        <div className="desktop-right">
-          <Section.Contact content={ content }/>
-          <Section.Summary content={ content }/>
-          <Section.Education content={ content }/>
-          <Section.Skills content={ content }/>
+        <div className="desktop">
+          <div className="left">
+            <Section.Header content={ content }/>
+            <Section.Employment content={ content }/>
+            <Section.Projects content={ content }/>
+          </div>
+          <div className="right">
+            <Section.Contact content={ content }/>
+            <Section.Summary content={ content }/>
+            <Section.Education content={ content }/>
+            <Section.Skills content={ content }/>
+          </div>
         </div>
       </main>
     );
