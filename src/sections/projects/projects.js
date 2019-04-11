195903926cbffa8a9fd24e4f8ecc0f10b5b93ccd
Carlo <thecarloferrer@gmail.com>
2019-04-11 11:57:18 -0400

    Resolve unique key warning for projects component

--- src/sections/projects/projects.js
+++ src/sections/projects/projects.js
@@ -3,10 +3,10 @@ import './projects.scss';
 
 class Projects extends Component {
   render() {
-    const projectsItems = this.props.content.projects.map((project) => {
-      const techItems = project.tech.map((tech) => <li>{tech}</li>);
+    const projectsItems = this.props.content.projects.map((project, parentIndex) => {
+      const techItems = project.tech.map((tech, childIndex) => <li key={`projectChild_${childIndex}`}>{tech}</li>);
 
-      return <li>
+      return <li key={`projectParent_${parentIndex}`}>
         <h3>{project.name}</h3>
         <ul>{techItems}</ul>
         <p>{project.description}</p>
