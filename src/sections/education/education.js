89d6d9a0df7c833f768357ba52014fc8a6ddf65d
Carlo <thecarloferrer@gmail.com>
2019-04-11 20:54:50 -0400

    Update education style

--- src/sections/education/education.js
+++ src/sections/education/education.js
@@ -5,8 +5,8 @@ class Education extends Component {
   render() {
     const educationItems = this.props.content.education.map((education, index) => <li className="item" key={ `education_${ index }` }>
       <h3 className="institution">{ education.institution }</h3>
-      <h4 className="discipline">{ education.discipline }</h4>
-      <h5 className="year">{ education.graduationYear }</h5>
+      <h4 className="discipline">{ education.discipline } <span className="year">({ education.graduationYear })</span></h4>
+      <h4 className="note">{ education.note }</h4>
     </li>);
 
     return (
