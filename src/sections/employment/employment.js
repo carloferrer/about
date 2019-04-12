03870a661336105737ab5373c427e1da059c54ef
Carlo <thecarloferrer@gmail.com>
2019-04-11 20:08:58 -0400

    Update employment style further

--- src/sections/employment/employment.js
+++ src/sections/employment/employment.js
@@ -11,8 +11,8 @@ class Employment extends Component {
           <h3 className="position">{ employment.position }</h3>
           <h4 className="employer">{ employment.employer }</h4>
           <h5 className="location">{ employment.location }</h5>
-          <h5 className="dates">{ employment.dateStart} - { employment.dateEnd }</h5>
         </hgroup>
+        <h5 className="dates">{ employment.dateStart} - { employment.dateEnd }</h5>
         <div className="duties">{ dutyItems }</div>
       </li>
     });
