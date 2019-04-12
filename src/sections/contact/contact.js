650266aa0edd0dee02dd257f18e776e4e58207eb
Carlo <thecarloferrer@gmail.com>
2019-04-12 13:35:40 -0400

    Apply links to contact info

--- src/sections/contact/contact.js
+++ src/sections/contact/contact.js
@@ -3,7 +3,24 @@ import './contact.scss';
 
 class Contact extends Component {
   render() {
-    const contactItems = Object.values(this.props.content.contact).map((info, index) => <li className="item" key={ `contact_${ index }` }>{ info }</li>);
+    // TODO: write better code
+    const { contact } = this.props.content;
+    const contactItems = Object.keys(this.props.content.contact).map((info, index) => {
+      let innerContactNode = contact[info];
+      switch (info) {
+        case 'email':
+          innerContactNode = <a className={ info } href={ `mailto:${ contact[info] }` }>{ contact[info] }</a>;
+          break;
+        case 'linkedin':
+        case 'github':
+          innerContactNode = <a className={ info } href={ `https://${ contact[info] }` }>{ contact[info] }</a>;
+          break;
+        default:
+          break;
+      }
+
+      return <li className="item" key={ `contact_${ index }` }>{ innerContactNode }</li>
+    });
 
     return (
       <address className="Contact">
