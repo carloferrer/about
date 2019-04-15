6ac03ad13c2884f0ec08a3115c2ab09910c39816
Carlo <thecarloferrer@gmail.com>
2019-04-15 16:55:37 -0400

    Reorganize assets

---
+++ src/assets/contact/index.js
@@ -0,0 +1,13 @@
+import github from './github.svg';
+import gmail from './gmail.svg';
+import linkedin from './linkedin.svg';
+import phone from './phone.svg';
+import pin from './pin.svg';
+
+export default {
+  github,
+  gmail,
+  linkedin,
+  phone,
+  pin
+};
